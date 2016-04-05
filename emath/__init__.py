0cabcf0be288bf6486f605cb028ae1157ef71026
iory <ab.ioryz@gmail.com>
2016-04-05 20:55:44 +0900

    Modified setup.cfg by using paster create

--- emath/__init__.py
+++ emath/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+from fibo import genFibo
+
 __author__  = "iory"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __licence__ = "MIT"
--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
 from setuptools import setup, find_packages
-from emath import __author__, __version__, __licence__
+from pip.req import parse_requirements
+import sys, os, pip
 
-setup(
-    name             = "emath",
-    version          = __version__,
-    description      = "Python Library for Number Theory",
-    license          = __licence__,
-    author           = __author__,
-    author_email     = "ab.ioryz@gmail.com",
-    url              = "hrrps://github.com/iory/emath.git",
-    packages         = find_packages(),
-    install_requires = [],
-)
+version = '0.0.2'
+
+requirements = [
+    str(requirement.req)
+    for requirement in parse_requirements('requirements.txt', session = pip.download.PipSession())
+]
+
+setup(name='emath',
+      version=version,
+      description="Python Library for Number Theory",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='iory',
+      author_email='ab.ioryz@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requirements,
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
